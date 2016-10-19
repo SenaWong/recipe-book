fcf3c0ca08276f2d540169a0c324626741205677
Edmund Wong <wongkokliong99@hotmail.com>
2016-10-19 23:54:48 +0800

    added ngFor directive

--- src/app/recipes/recipe-list.component.ts
+++ src/app/recipes/recipe-list.component.ts
@@ -7,9 +7,10 @@ import { RecipeItemComponent } from './recipe-item.component';
   templateUrl: './recipe-list.component.html'
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [];
+  recipes: Recipe[] = [new Recipe("French Fries", "Lightly salted fries fried to a golden perfection", "http://s2.dmcdn.net/IZlI9/1280x720-ts8.jpg"),
+                       new Recipe("Fried Chicken", "Crispy fried chicken marinated in secret herbs and spices","http://web.gastongazette.com/interactive/friedchicken/images/popeyeschicken.png" )
+                      ];
   @Output() recipeSelected = new EventEmitter<Recipe>();
-  rez = new Recipe("French Fries", "Lightly salted fries fried to a golden perfection", "http://s2.dmcdn.net/IZlI9/1280x720-ts8.jpg")
 
   onSelected(recipe: Recipe) {
     this.recipeSelected.emit(recipe)
