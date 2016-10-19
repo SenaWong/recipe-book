766730ebfeb6966daab4c71ba3d094d865e7e940
Edmund Wong <wongkokliong99@hotmail.com>
2016-10-19 20:49:36 +0800

    added recipe description

--- src/app/recipes/recipe-list.component.ts
+++ src/app/recipes/recipe-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from './recipe';
+import { RecipeItemComponent } from './recipe-item.component';
 
 @Component({
   selector: 'rb-recipe-list',
@@ -7,7 +8,12 @@ import { Recipe } from './recipe';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [];
+  @Output() recipeSelected = new EventEmitter<Recipe>();
   rez = new Recipe("French Fries", "Lightly salted fries fried to a golden perfection", "http://s2.dmcdn.net/IZlI9/1280x720-ts8.jpg")
+
+  onSelected(recipe: Recipe) {
+    this.recipeSelected.emit(recipe)
+  }
 
   constructor() { }
 
