581a084b4631862ad88ef495594c090514d34e35
Edmund Wong <wongkokliong99@hotmail.com>
2016-10-26 23:35:22 +0800

    Added some dummy ingredients

--- src/app/recipe.service.ts
+++ src/app/recipe.service.ts
@@ -6,11 +6,13 @@ import { Ingredient } from './shared/ingredient';
 export class RecipeService {
 
   private recipes: Recipe[] = [new Recipe("French Fries", "Lightly salted fries fried to a golden perfection", "http://s2.dmcdn.net/IZlI9/1280x720-ts8.jpg", [
-    new Ingredient('French Fries', 2),
-    new Ingredient('Pork', 2)
-    ]),
-                       new Recipe("Fried Chicken", "Crispy fried chicken marinated in secret herbs and spices","http://web.gastongazette.com/interactive/friedchicken/images/popeyeschicken.png", new Ingredient('Chicken', 2))
-                      ];
+                                 new Ingredient('French Fries', 2),
+                                 new Ingredient('Pork', 2)]),
+
+                              new Recipe("Fried Chicken", "Crispy fried chicken marinated in secret herbs and spices","http://web.gastongazette.com/interactive/friedchicken/images/popeyeschicken.png",[
+                                 new Ingredient('Chicken', 2),
+                                 new Ingredient('Special Spice', 3)])
+                            ];
 
 
   getRecipes() {
